1029114150fce07d45a6adf6bc6054fd296d27a5
Adrian Rodriguez <rodriguezcadr@gmail.com>
2024-11-17 19:44:57 -0300

    updating documents

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import os
 import json
-from pymongo import MongoClient
+from ftplib import FTP
 from dotenv import load_dotenv
+from pymongo import MongoClient
 
 load_dotenv()
 
@@ -10,7 +11,12 @@ MONGO_DB_NAME = os.getenv("MONGO_DB_NAME")
 MONGO_USER = os.getenv("MONGO_USER")
 MONGO_PASSWORD = os.getenv("MONGO_PASSWORD")
 
-OUTPUT_FOLDER = "documents"
+FTP_HOST = os.getenv("FTP_HOST")
+FTP_USER = os.getenv("FTP_USER")
+FTP_PASSWORD = os.getenv("FTP_PASSWORD")
+
+DOCUMENTS_FOLDER = "documents"
+DOCUMENTS_HOST = "/public_html/data"
 
 
 def connect_to_mongo():
@@ -44,10 +50,29 @@ def export_collections(client, collections, output_folder):
         print(f"Error: {e}")
 
 
+def upload_to_ftp(local_folder, remote_folder):
+    try:
+        ftp = FTP(FTP_HOST)
+        ftp.login(FTP_USER, FTP_PASSWORD)
+        ftp.cwd(remote_folder)
+
+        for filename in os.listdir(local_folder):
+            if filename.endswith(".json"):
+                local_filepath = os.path.join(local_folder, filename)
+                with open(local_filepath, "rb") as file:
+                    ftp.storbinary(f"STOR {filename}", file)
+                print(f"Updated {filename}.")
+
+        ftp.quit()
+    except Exception as e:
+        print(f"Error: {e}")
+
+
 def main():
     client = connect_to_mongo()
     collections = ["products", "categories", "subcategories"]
-    export_collections(client, collections, OUTPUT_FOLDER)
+    export_collections(client, collections, DOCUMENTS_FOLDER)
+    upload_to_ftp(DOCUMENTS_FOLDER, DOCUMENTS_HOST)
 
 
 if __name__ == "__main__":
